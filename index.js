f72cb39c7ef43442d2d845844cda1ad344d9e0de
nibras <nibrasnib1526@gmail.com>
2023-12-16 18:36:38 +0600

    changes in index file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require('dotenv').config()
+console.log(process.env.PORT)
+
+const abc = {
+    "name":"nibras",
+    "age": 30,
+    "email": "nibrasnib1627@gmail.com"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,6 +18,9 @@ app.get('/nibras', (req,res)=>{
     res.send('from nibras page')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get("/my_json", (req,res)=>{
+    res.send(abc);
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
